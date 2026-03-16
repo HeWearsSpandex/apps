dec349b79fa40ea3b783103a5c7f8272110d760d
HeWearsSpandex <wayne@jhbukltd.co.uk>
2026-03-16 19:31:16 +0000

    Create people.js

---
+++ bonus-metres/js/people.js
@@ -0,0 +1,11 @@
+const PEOPLE = [
+  'Adrian Lutic','Alex Nita','Antun Vidakovic','Bradley Turnstill',
+  'Byron Claridge','Callum Evans','Cameron Gill','Christopher Scourfield','Craig Dolan',
+  'Edward Shanley','Gary Mounce','George Norris','James Hickman','James Hodge-Brooks',
+  'James Whiteoak','Jonathan Thompson','Joseph Nixon','Joshua Powell','Joshua Rowberry',
+  'Lewis Cannon','Liam Hammon','Luke Piller','Mark Sneap','Morgan Hemming',
+  'Nicholas Kingshott','Richard Clanford','Robert Tamas','Ryan Breslan','Ryan Manners',
+  'Tom Brooks','Tristan Allen','Tyler Webb','David Mahon','Jason Bligh','Rhys Grant',
+  'Ross Wills','Ross Striplin','Jordan Reynolds','Mark Stafford','William Garratt',
+  'Chris Voisey','Ryan Turnstil','Jenna Turvey','Archie Beckett','Colby Mathews'
+];
